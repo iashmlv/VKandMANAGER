d104edc520b163b3fe507bfad41e28e7b1a96268
Ivan Shumilov <ivan.shumilov91@gmail.com>
2020-06-19 18:50:07 +0300

    added manager task

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,19 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+
+    public Post[] getPosts() {
+        return posts;
+    }
+
+    public Post[] search(int ownerId, String domain, String query, int ownersOnly, int count, int offset) {
+        return null;
+    }
+
+    public void delete(int postId, int ownerId) {
+        // not returning anything
+    }
+}
